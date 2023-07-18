08fb80734111f929f171ba40e056784ae14cbdf4
Wangoru Kihara <soloincc@gmail.com>
2023-07-18 09:02:41 +0300

    Remove extra print

--- models.py
+++ models.py
@@ -357,7 +357,6 @@ class Profile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
-    print(instance)
     if created:
         Profile.objects.create(user=instance)
     # instance.profile.save()
